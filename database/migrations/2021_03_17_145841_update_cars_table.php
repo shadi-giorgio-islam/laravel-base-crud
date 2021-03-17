cd917fa85371420f603b327f366873a759a79e7f
Giorgio <shadigiorgioislam@gmail.com>
2021-03-17 16:25:36 +0100

    Add migration, model and controller

---
+++ database/migrations/2021_03_17_145841_update_cars_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cars', function (Blueprint $table) {
+            $table->string('motore')->nullable()->after('prezzo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cars', function (Blueprint $table) {
+            $table->dropcolumn('motore');
+        });
+    }
+}
